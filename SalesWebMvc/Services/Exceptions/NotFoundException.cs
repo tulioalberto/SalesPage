48273941de80ae39495c4972e6ad8428a0248f8a
unknown <tulio.koca@gmail.com>
2022-08-05 17:40:24 -0300

    Personalized exceptions on my projects learned by my seld

---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions {
+    public class NotFoundException : ApplicationException {
+        public NotFoundException(String message) : base(message) {
+
+        }
+    }
+}
